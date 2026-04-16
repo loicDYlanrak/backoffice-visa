c0155714873450e741e5cb4d82185da26886c25d
erica-niriantsoa <erica.ny.riantsoa@gmail.com>
2026-04-16 16:10:34 +0200

    update database

--- script/16-04-2026.sql
+++ script/16-04-2026.sql
@@ -1,9 +1,12 @@
+<<<<<<< Updated upstream
 
 CREATE TABLE Genre (
     id INT AUTO_INCREMENT PRIMARY KEY,
     libelle VARCHAR(50) NOT NULL
 );
 
+=======
+>>>>>>> Stashed changes
 CREATE TABLE Nationalite (
     id INT AUTO_INCREMENT PRIMARY KEY,
     libelle VARCHAR(50) NOT NULL
@@ -19,28 +22,57 @@ CREATE TABLE type_visa (
     libelle VARCHAR(50) NOT NULL  -- 'investisseur', 'travailleur'
 );
 
-CREATE TABLE Statut_demande (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(50) NOT NULL  -- 'brouillon', 'soumise', 'en_cours', 'validee', 'rejetee'
-);
 
 CREATE TABLE Passeport (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    id_demandeur INT NOT NULL,
     numero_passeport VARCHAR(50) NOT NULL UNIQUE,
     date_delivrance DATE NOT NULL,
     date_expiration DATE NOT NULL,
-    pays_delivrance VARCHAR(100)
+    pays_delivrance VARCHAR(100),
+    FOREIGN KEY (id_demandeur) REFERENCES Demandeur(id)
+);
+
+CREATE TABLE Statut_passeport (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_passeport INT NOT NULL,
+    statut INT NOT NULL ,-- 'actif', 'expire', 'perdu', 'volee'
+    date_changement_statut DATE,
+    FOREIGN KEY (id_passeport) REFERENCES Passeport(id)
+);
+
+CREATE TABLE Visa_transformable(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_demandeur INT NOT NULL,
+    id_passeport INT NOT NULL,
+    numero_reference VARCHAR(50) NOT NULL UNIQUE,
+    FOREIGN KEY (id_demandeur) REFERENCES Demandeur(id),
+    FOREIGN KEY (id_passeport) REFERENCES Passeport(id)
 );
 
 CREATE TABLE Visa (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    numero_visa VARCHAR(50),
+    id_demande INT NOT NULL,
+    reference VARCHAR(50),
     date_debut DATE NOT NULL,
     date_fin DATE NOT NULL,
-    transformable BOOLEAN DEFAULT TRUE,
-    id_type_visa INT NOT NULL,
     id_passeport INT NOT NULL,
+<<<<<<< Updated upstream
     FOREIGN KEY (id_type_visa) REFERENCES type_visa(id),
+=======
+    FOREIGN KEY (id_passeport) REFERENCES Passeport(id),
+    FOREIGN KEY (id_demande) REFERENCES Demande(id)
+);
+
+CREATE TABLE carte_resident(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_demande INT NOT NULL,
+    reference VARCHAR(50),
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    id_passeport INT NOT NULL,
+    FOREIGN KEY (id_demande) REFERENCES Demande(id),
+>>>>>>> Stashed changes
     FOREIGN KEY (id_passeport) REFERENCES Passeport(id)
 );
 
@@ -48,78 +80,56 @@ CREATE TABLE Demandeur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
-    nom_jeune_fille VARCHAR(50),
     date_naissance DATE NOT NULL,
     lieu_naissance VARCHAR(100) NOT NULL,
-    profession VARCHAR(100) NOT NULL,
     telephone VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
     adresse TEXT NOT NULL,
     
     id_situation_familiale INT NOT NULL,
-    id_nationalite_actuelle INT NOT NULL,
-    id_nationalite_origine INT NOT NULL,
-    id_genre INT NOT NULL,
-    
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id_nationalite INT NOT NULL,
     
     FOREIGN KEY (id_situation_familiale) REFERENCES Situation_familiale(id),
-    FOREIGN KEY (id_nationalite_actuelle) REFERENCES Nationalite(id),
-    FOREIGN KEY (id_nationalite_origine) REFERENCES Nationalite(id),
-    FOREIGN KEY (id_genre) REFERENCES Genre(id)
+    FOREIGN KEY (id_nationalite) REFERENCES Nationalite(id)
 );
 
-CREATE TABLE Document_requis (
+CREATE TABLE Type_demande (
     id INT AUTO_INCREMENT PRIMARY KEY,
+<<<<<<< Updated upstream
     libelle VARCHAR(255) NOT NULL,
     est_commun BOOLEAN DEFAULT FALSE,  -- TRUE = pièce commune
     id_type_visa INT NULL,              -- NULL = commun, sinon spécifique au type
     FOREIGN KEY (id_type_visa) REFERENCES type_visa(id)
+=======
+    libelle VARCHAR(50) NOT NULL  
+>>>>>>> Stashed changes
 );
+
 
 CREATE TABLE Demande (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    reference VARCHAR(50) UNIQUE NOT NULL,  -- Référence unique pour recherche
+    id_visa_transformable INT NOT NULL,
     date_demande DATE NOT NULL,
     id_statut INT NOT NULL,
     id_demandeur INT NOT NULL,
-    id_visa INT NOT NULL,
-    observations TEXT,
+    id_type_visa INT NOT NULL,
+    id_type_demande INT NOT NULL,
     date_traitement DATE,
-    motif_rejet TEXT,
-    
-    FOREIGN KEY (id_statut) REFERENCES Statut_demande(id),
+    FOREIGN KEY (id_type_demande) REFERENCES Type_demande(id),
     FOREIGN KEY (id_demandeur) REFERENCES Demandeur(id),
-    FOREIGN KEY (id_visa) REFERENCES Visa(id)
-);
-/* 
-CREATE TABLE Demande_document (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_demande INT NOT NULL,
-    id_document_requis INT NOT NULL,
-    est_fourni BOOLEAN DEFAULT FALSE,
-    chemin_fichier VARCHAR(255),
-    date_upload TIMESTAMP NULL,
-    
-    FOREIGN KEY (id_demande) REFERENCES Demande(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_document_requis) REFERENCES Document_requis(id),
-    UNIQUE KEY unique_demande_document (id_demande, id_document_requis)
-); */
-
-CREATE TABLE Histo_statut_demande (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_demande INT NOT NULL,
-    id_statut_ancien INT,
-    id_statut_nouveau INT NOT NULL,
-    date_changement TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    commentaire VARCHAR(255),
-    
-    FOREIGN KEY (id_demande) REFERENCES Demande(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_statut_ancien) REFERENCES Statut_demande(id),
-    FOREIGN KEY (id_statut_nouveau) REFERENCES Statut_demande(id)
+    FOREIGN KEY (id_type_visa) REFERENCES Type_visa(id),
+    FOREIGN KEY (id_visa_transformable) REFERENCES Visa_transformable(id)
 );
 
+CREATE TABLE Statut_demande (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_demande INT NOT NULL,
+    statut INT NOT NULL,-- 'brouillon', 'soumise', 'en_cours', 'validee', 'rejetee'
+    date_changement_statut DATE,
+    FOREIGN KEY (id_demande) REFERENCES Demande(id)
+);
+
+<<<<<<< Updated upstream
 CREATE TABLE Demandeur_Passeport (
     id_demandeur INT NOT NULL,
     id_passeport INT NOT NULL,
@@ -162,3 +172,5 @@ INSERT INTO Document_requis (libelle, est_commun, id_type_visa) VALUES
 ('Autorisation emploi délivrée à Madagascar par le Ministère de la Fonction publique', FALSE, (SELECT id FROM type_visa WHERE libelle = 'travailleur')),
 ('Attestation d''emploi délivré par l''employeur (Original)', FALSE, (SELECT id FROM type_visa WHERE libelle = 'travailleur'));
 
+=======
+>>>>>>> Stashed changes
