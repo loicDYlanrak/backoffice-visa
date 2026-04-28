b7ea5bfd039ac781c592b92cb3824f7fcce72cc2
Loic Dylan Rakotoarivony <loicrakotoarivony07@gmail.com>
2026-04-28 11:34:42 +0300

    Update DemandeService.java

--- src/main/java/com/project/visa/service/DemandeService.java
+++ src/main/java/com/project/visa/service/DemandeService.java
@@ -1,16 +1,14 @@
 package com.project.visa.service;
 
-import com.project.visa.entity.DemandeEntity;
-import com.project.visa.entity.DemandeurEntity;
-import com.project.visa.entity.PasseportEntity;
-import com.project.visa.entity.StatutDemandeEntity;
-import com.project.visa.entity.VisaTransformableEntity;
-import com.project.visa.repository.DemandeRepository;
+import com.project.visa.entity.*;
+import com.project.visa.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class DemandeService {
@@ -19,6 +17,11 @@ public class DemandeService {
     private DemandeRepository demandeRepository;
     @Autowired
     private StatutDemandeService statutDemandeService;
+    @Autowired
+    private PieceDemandeRepository pieceDemandeRepository;
+
+    @Autowired
+    private PieceRepository pieceRepository;
 
     public List<DemandeEntity> findAll() {
         return demandeRepository.findAll();
@@ -66,6 +69,40 @@ public class DemandeService {
         }
         return dernierStatut.getStatut() != StatutDemandeEntity.STATUT_APPROUVE &&
                 dernierStatut.getStatut() != StatutDemandeEntity.STATUT_REJETER;
+    }
+
+    public boolean canBeScan(DemandeEntity demande) {
+       
+        if (demande == null || demande.getVisaTransformable() == null) {
+            return false;
+        }
+
+        Integer typeVisaId = demande.getTypeVisa().getId();
+
+        List<PieceDemandeEntity> piecesUploaded = pieceDemandeRepository.findByDemandeId(demande.getId());
+        Set<Integer> uploadedPieceIds = piecesUploaded.stream()
+                .map(pd -> pd.getPiece().getId())
+                .collect(Collectors.toSet());
+
+        List<PieceEntity> piecesGenerales = pieceRepository.findAll().stream()
+                .filter(p -> p.getTypeVisa() == null)
+                .collect(Collectors.toList());
+
+        for (PieceEntity pieceGenerale : piecesGenerales) {
+            if (!uploadedPieceIds.contains(pieceGenerale.getId())) {
+                return false; 
+            }
+        }
+
+        List<PieceEntity> piecesSpecifiques = pieceRepository.findByTypeVisaEntity_Id(typeVisaId);
+
+        for (PieceEntity pieceSpecifique : piecesSpecifiques) {
+            if (!uploadedPieceIds.contains(pieceSpecifique.getId())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
