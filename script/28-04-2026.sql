de8e0feb99f480953eef6bd7c9f315501be7bc98
Loic Dylan Rakotoarivony <loicrakotoarivony07@gmail.com>
2026-04-28 11:21:00 +0300

    Update 28-04-2026.sql

--- script/28-04-2026.sql
+++ script/28-04-2026.sql
@@ -5,3 +5,11 @@ CREATE TABLE scan_fichier {
     date_upload date not null,
     FOREIGN key (id_piece_demande) references piece_demande(id) 
 };
+
+CREATE TABLE statut_visa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_visa INT NOT NULL,
+    statut INT NOT NULL,-- 'non valide : 1', 'creer : 5', 'approuve : 30', ' rejeter : 40 '
+    date_changement_statut DATE,
+    FOREIGN KEY (id_visa) REFERENCES visa(id)
+);
