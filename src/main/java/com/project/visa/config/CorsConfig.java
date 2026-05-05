793038f4f172df29b450b4a39dbe9d50d1f6a81f
erica-niriantsoa <erica.ny.riantsoa@gmail.com>
2026-05-05 07:51:49 +0200

    fix:CorsConfig

--- src/main/java/com/project/visa/config/CorsConfig.java
+++ src/main/java/com/project/visa/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.project.visa.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,11 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:./uploads/");
     }
 }
